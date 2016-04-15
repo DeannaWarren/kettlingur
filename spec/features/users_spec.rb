1298125e645f7b4075eb2d12f5f867eafe2ad0a8
deannawarren <mrs.deanna.warren@gmail.com>
2016-04-14 21:56:37 -0700

    Work on feature tests for User actions

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User", type: :feature, js: true do
 
-  describe "without an account" do
+  describe "who is signed out" do
     it "can create an account and be signed in" do
       visit root_path
       click_link("Sign Up")
@@ -13,42 +13,42 @@ RSpec.feature "User", type: :feature, js: true do
       click_button("Create Your Account")
       expect(page).to have_content("Welcome, user@email.com!")
     end
+    it "cannot see the sign out option" do
+      visit root_path
+      expect(page).to_not have_button("Sign Out")
+    end
+    it "can sign in" do
+      user_details = {
+        username: "username",
+        email: "user@email.com",
+        password: "password"
+      }
+      User.create!(user_details)
+      visit root_path
+      click_link("Sign In")
+      fill_in "Email", with: "user@email.com"
+      fill_in "Password", with: "password"
+      click_on("Log in")
+      expect(page).to have_content("Welcome, user@email.com!")
+    end
   end
 
-  describe "with an account" do
-    context "who is is signed out" do
-      it "can sign in" do
-        user_details = {
-          username: "username",
-          email: "user@email.com",
-          password: "password"
-        }
-        User.create!(user_details)
-        visit root_path
-        click_link("Sign In")
-        fill_in "Email", with: "user@email.com"
-        fill_in "Password", with: "password"
-        click_on("Log in")
-        expect(page).to have_content("Welcome, user@email.com!")
-      end
-    end # end signed out context
-    context "who is signed in " do
-      it "can sign out" do
-        user_details = {
-          username: "username",
-          email: "user@email.com",
-          password: "password"
-        }
-        User.create!(user_details)
-        visit root_path
-        click_link("Sign In")
-        fill_in "Email", with: "user@email.com"
-        fill_in "Password", with: "password"
-        click_on("Log in")
-        click_on("Sign Out")
-        expect(page).to have_content("Sign In")
-      end
-    end # context is signed in
+  describe "who is signed in" do
+    # it "can sign out" do
+    #   user_details = {
+    #     username: "username",
+    #     email: "user@email.com",
+    #     password: "password"
+    #   }
+    #   User.create!(user_details)
+    #   visit root_path
+    #   click_link("Sign In")
+    #   fill_in "Email", with: "user@email.com"
+    #   fill_in "Password", with: "password"
+    #   click_on("Log in")
+    #   click_on("Sign Out")
+    #   expect(page).to have_content("Sign In")
+    # end
   end
 
 end
